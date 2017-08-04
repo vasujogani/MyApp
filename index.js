5705403012178208bfacf1a03a4a1363767bbacf
Vasu Jogani <vasujogani@Vasus-MBP.austin.rr.com>
2017-08-04 13:27:59 -0500

    updated app.js with express

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-var http = require('http');
-var dt = require('./timemodule.js');
-var url = require('url');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-// var events = require('events');
-// var eventEmitter = new events.EventEmitter();
-// eventEmitter.on('scream', function () {console.log('I hear a scream!');});
-// eventEmitter.emit('scream');
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
-http.createServer(function (req, res) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    var q = url.parse(req.url, true).query;
-    res.write('Current time is :' + dt.myDateTime() + "\n");
-    var txt = q.year + " " + q.month;
-//    res.write(req.url + "\n");
-    res.write(txt);
-    res.end();
-}).listen(8080);
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+});
+
+app.use(express.static('public'));
+app.get('/form.html', function(req, res){
+  res.sendFile(__dirname + "/" + "form.html");
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello GET');
+});
+
+app.post('/process',urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+app.get('/process',urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+app.delete('/del', function (req, res) {
+  console.log('Got a DELETE request');
+  res.send('Hello DELETE');
+});
+
+app.get('/abcd*', function(req, res) {
+  console.log("Got a GET request for /ab*cd");
+  res.send('Page Pattern Match');
+});
